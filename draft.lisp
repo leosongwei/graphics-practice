93a255b5e934ba52d42cebacccf37fd170c11d1b
SongWei <leo_songwei@outlook.com>
2018-11-21 20:59:59 +1100

    sdl

---
+++ draft.lisp
@@ -0,0 +1,48 @@
+(load "math.lisp")
+(load "glbind.lisp")
+
+(c-sdl-init +sdl_init_video+)
+(c-sdl-gl-setattribute +sdl_gl_context_major_version+ 3)
+(c-sdl-gl-setattribute +sdl_gl_context_major_version+ 3)
+(c-sdl-gl-setattribute +sdl_gl_context_profile_mask+ +sdl_gl_context_profile_core+)
+
+(defparameter *window*
+  (cffi:with-foreign-string (title "test")
+    (c-sdl-createwindow title +sdl_windowpos_undefined+ +sdl_windowpos_undefined+
+                        640 480
+                        (logior +sdl_window_opengl+ +sdl_window_shown+))))
+
+(defparameter *glcontext*
+  (c-sdl-gl-createcontext *window*))
+
+(c-glewinit)
+(c-gl-enable +gl_depth_test+)
+(c-gl-depthfunc +gl_less+)
+(c-glClearColor 0.0 0.0 0.4 0.0)
+
+(c-glClear (logior +GL_COLOR_BUFFER_BIT+ +GL_DEPTH_BUFFER_BIT+))
+(c-sdl-gl-swapwindow *window*)
+
+(progn
+  (c-glClearColor 0.0 0.0 0.4 0.0)
+  (c-glClear +GL_COLOR_BUFFER_BIT+))
+(progn
+  (c-glfwPollEvents)
+  (c-glfwswapbuffers *window*))
+
+(defparameter *triangle-points-buffer*
+  (float-buffer
+   #(-1.0 -1.0 0.0
+     1.0  -1.0 0.0
+     0.0  1.0  0.0)))
+
+(progn
+  (defparameter *vertex-buffer* (glgenbuffer-1))
+  (c-glbindbuffer +GL_ARRAY_BUFFER+ *vertex-buffer*)
+  (c-glbufferdata +GL_ARRAY_BUFFER+ (* 4 9) *triangle-points-buffer* +gl_static_draw+))
+
+(c-glenablevertexattribarray 0)
+(c-glbindbuffer +GL_ARRAY_BUFFER+ *vertex-buffer*)
+(c-glVertexAttribPointer 0 3 +GL_FLOAT+ +GL-FALSE+ 0 null-pointer)
+
+(c-sdl-gl-swapwindow *window*)
--- glbind.lisp
+++ glbind.lisp
@@ -1,80 +1,118 @@
 (ql:quickload 'cffi)
 
 (cffi:use-foreign-library "libGLEW.so")
-(cffi:use-foreign-library "libglfw.so")
+(cffi:use-foreign-library "libGLU.so")
+(cffi:use-foreign-library "libGL.so")
+(cffi:use-foreign-library "libGLX.so")
+(cffi:use-foreign-library "libSDL2.so")
+
+;; GLenum glewErr = glewInit();
+(cffi:defcfun (c-glewinit "glewInit") :uint)
 
 (defun float-buffer (float-array)
   (cffi:foreign-alloc :float :initial-contents float-array))
 ;;(cffi:mem-aref (float-buffer #(1.0 2.0 3.0 4.0)) :float 1) => 2.0
 
-(defparameter *error-message* nil)
-(defparameter *error-code* nil)
-;;void ErrorCallback(int, const char* err_str)
-(cffi:defcallback glew-error-callback :void ((error-code :int) (string :pointer))
-  (let ((message (cffi:foreign-string-to-lisp string)))
-    (setf *error-message* message)
-    (setf *error-code* (cffi:mem-ref error-code :int))))
-
-(cffi:defcfun (c-glfwSetErrorCallback "glfwSetErrorCallback") :pointer
-  (function :pointer))
-
-(c-glfwseterrorcallback (cffi:callback glew-error-callback))
-
-
-
-;; int glfwInit(void)
-(cffi:defcfun (c-glfwinit "glfwInit") :int)
-(defun glfwinit ()
-  (if (= 0 (c-glfwinit))
-      (progn (format t "Failed to initialize GLFW~%")
-             nil)
-      t))
-
 (defparameter null-pointer (cffi:null-pointer))
-(defparameter +GL_FLOAT+ #x1406)
 
+(defmacro defenum (&body list)
+  (append
+   '(progn)
+   (let ((params '()))
+     (dotimes (i (length list))
+       (push `(defparameter ,(nth i list) ,i) params))
+     params)))
+
+(defenum
+  +SDL_GL_RED_SIZE+
+  +SDL_GL_GREEN_SIZE+
+  +SDL_GL_BLUE_SIZE+
+  +SDL_GL_ALPHA_SIZE+
+  +SDL_GL_BUFFER_SIZE+
+  +SDL_GL_DOUBLEBUFFER+
+  +SDL_GL_DEPTH_SIZE+
+  +SDL_GL_STENCIL_SIZE+
+  +SDL_GL_ACCUM_RED_SIZE+
+  +SDL_GL_ACCUM_GREEN_SIZE+
+  +SDL_GL_ACCUM_BLUE_SIZE+
+  +SDL_GL_ACCUM_ALPHA_SIZE+
+  +SDL_GL_STEREO+
+  +SDL_GL_MULTISAMPLEBUFFERS+
+  +SDL_GL_MULTISAMPLESAMPLES+
+  +SDL_GL_ACCELERATED_VISUAL+
+  +SDL_GL_RETAINED_BACKING+
+  +SDL_GL_CONTEXT_MAJOR_VERSION+
+  +SDL_GL_CONTEXT_MINOR_VERSION+
+  +SDL_GL_CONTEXT_EGL+
+  +SDL_GL_CONTEXT_FLAGS+
+  +SDL_GL_CONTEXT_PROFILE_MASK+
+  +SDL_GL_SHARE_WITH_CURRENT_CONTEXT+
+  +SDL_GL_FRAMEBUFFER_SRGB_CAPABLE+
+  +SDL_GL_CONTEXT_RELEASE_BEHAVIOR+
+  +SDL_GL_CONTEXT_RESET_NOTIFICATION+
+  +SDL_GL_CONTEXT_NO_ERROR+)
+(defparameter +SDL_INIT_VIDEO+ #x00000020)
+
+(defparameter +SDL_GL_CONTEXT_PROFILE_CORE+ #x0001)
+(defparameter +SDL_GL_CONTEXT_PROFILE_COMPATIBILITY+ #x0002)
+(defparameter +SDL_GL_CONTEXT_PROFILE_ES+ #x0004)
+
+(defparameter +SDL_WINDOW_FULLSCREEN+ #x00000001)
+(defparameter +SDL_WINDOW_OPENGL+ #x00000002)
+(defparameter +SDL_WINDOW_SHOWN+ #x00000004)
+(defparameter +SDL_WINDOW_HIDDEN+ #x00000008)
+(defparameter +SDL_WINDOW_BORDERLESS+ #x00000010)
+(defparameter +SDL_WINDOW_RESIZABLE+ #x00000020)
+(defparameter +SDL_WINDOW_MINIMIZED+ #x00000040)
+(defparameter +SDL_WINDOW_MAXIMIZED+ #x00000080)
+(defparameter +SDL_WINDOW_INPUT_GRABBED+ #x00000100)
+(defparameter +SDL_WINDOW_INPUT_FOCUS+ #x00000200)
+(defparameter +SDL_WINDOW_MOUSE_FOCUS+ #x00000400)
+(defparameter +SDL_WINDOW_FULLSCREEN_DESKTOP+ (logior +SDL_WINDOW_FULLSCREEN+ #x00001000))
+(defparameter +SDL_WINDOW_FOREIGN+ #x00000800)
+(defparameter +SDL_WINDOW_ALLOW_HIGHDPI+ #x00002000)
+
+;;#define SDL_WINDOWPOS_UNDEFINED_MASK    0x1FFF0000u
+;;#define SDL_WINDOWPOS_UNDEFINED_DISPLAY(X)  (SDL_WINDOWPOS_UNDEFINED_MASK|(X))
+;;#define SDL_WINDOWPOS_UNDEFINED         SDL_WINDOWPOS_UNDEFINED_DISPLAY(0)
+(defparameter +SDL_WINDOWPOS_UNDEFINED_MASK+ #x1FFF0000)
+(defparameter +SDL_WINDOWPOS_UNDEFINED+ +SDL_WINDOWPOS_UNDEFINED_MASK+)
+
+;; int SDL_Init(Uint32 flags)
+(cffi:defcfun (c-sdl-init "SDL_Init") :int (flags :uint))
+
+;; int SDL_GL_SetAttribute(SDL_GLattr attr, int value)
+(cffi:defcfun (c-sdl-gl-setattribute "SDL_GL_SetAttribute") :int
+  (attr :uchar) (value :int))
+
+(defun init-sdl-gl-attribute ()
+  (c-sdl-gl-setattribute +sdl_gl_context_major_version+ 3)
+  (c-sdl-gl-setattribute +sdl_gl_context_minor_version+ 3)
+  (c-sdl-gl-setattribute +SDL_GL_CONTEXT_PROFILE_MASK+ +SDL_GL_CONTEXT_PROFILE_CORE+))
+
+;; SDL_Window* SDL_CreateWindow(const char* title, int x, int y, int w, int h, Uint32 flags)
+(cffi:defcfun (c-sdl-createwindow "SDL_CreateWindow") :pointer
+  (title :pointer) (x :int) (y :int) (w :int) (h :int) (flags :uint))
+
+;; SDL_GLContext SDL_GL_CreateContext(SDL_Window* window)
+;; typedef void *SDL_GLContext;
+(cffi:defcfun (c-sdl-gl-createcontext "SDL_GL_CreateContext") :pointer
+  (window :pointer))
+
+;; void SDL_GL_SwapWindow(SDL_Window* window)
+(cffi:defcfun (c-sdl-gl-swapwindow "SDL_GL_SwapWindow") :void
+  (window :pointer))
+
+;; ---------OpenGL------------
+(defparameter +GL_FLOAT+ #x1406)
 (defparameter +GL-TRUE+ 1)
 (defparameter +GL-FALSE+ 0)
-(defparameter +GLFW-TRUE+ 1)
-(defparameter +GLFW-FALSE+ 0)
-
-(defparameter +GLFW-SAMPLES+ #x0002100D)
-(defparameter +GLFW-CONTEXT-VERSION-MAJOR+ #x00022002)
-(defparameter +GLFW-CONTEXT-VERSION-MINOR+ #x00022003)
-(defparameter +GLFW-OPENGL-FORWARD-COMPAT+ #x00022006)
-(defparameter +GLFW-OPENGL-PROFILE+ #x00022008)
-(defparameter +GLFW-OPENGL-CORE-PROFILE+ #x00032001)
-
 (defparameter +GL_STATIC_DRAW+ #x88E4)
 (defparameter +GL_ARRAY_BUFFER+ #x8892)
 (defparameter +GL_COLOR_BUFFER_BIT+ #x00004000)
-
-
-
-
-(cffi:defcfun (c-glfwWindowHint "glfwWindowHint") :void
-  (hint :int) (value :int))
-
-;; https://www.glfw.org/faq.html#41---how-do-i-create-an-opengl-30-context
-(defun init-glew-and-context ()
-  (if (not (glfwinit)) (error "glfwinit failed"))
-  (c-glfwwindowhint +glfw-samples+ 2)
-  (c-glfwwindowhint +glfw-context-version-major+ 3)
-  (c-glfwwindowhint +glfw-context-version-minor+ 3)
-  (c-glfwwindowhint +glfw-opengl-forward-compat+ +gl-true+)
-  (c-glfwwindowhint +glfw-opengl-profile+ +glfw-opengl-core-profile+))
-
-(cffi:defcfun (c-glfwCreateWindow "glfwCreateWindow") :pointer
-  (width :int) (height :int) (title :pointer) (monitor :pointer) (share :pointer))
-
-;; void glfwMakeContextCurrent 	( 	GLFWwindow *  	window	)
-(cffi:defcfun (c-glfwMakeContextCurrent "glfwMakeContextCurrent") :void
-  (window :pointer))
-
-(cffi:defcfun (c-glfwPollEvents "glfwPollEvents") :void)
-
-(cffi:defcfun (c-glfwSwapBuffers "glfwSwapBuffers") :void
-  (window :pointer))
+(defparameter +GL_DEPTH_BUFFER_BIT+ #x00000100)
+(defparameter +GL_LESS+ #x0201)
+(defparameter +GL_DEPTH_TEST+ #x0B71)
 
 ;; typedef int GLsizei;
 ;; typedef unsigned int GLuint;
@@ -121,4 +159,8 @@
 (cffi:defcfun (c-glClearColor "glClearColor") :void
   (red :float) (green :float) (blue :float) (alpha :float))
 
+;; void glEnable(GLenum cap);
+(cffi:defcfun (c-gl-enable "glEnable") :void (cap :uint))
 
+;; void glDepthFunc(GLenum func);
+(cffi:defcfun (c-gl-depthfunc "glDepthFunc") :void (func :uint))
