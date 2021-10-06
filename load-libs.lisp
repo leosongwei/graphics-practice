c478ce36c6d8550b70faaa81b7bb525630ea4b52
leo_songwei <leo_songwei@outlook.com>
2021-10-06 22:52:03 +1100

    load-libs

---
+++ load-libs.lisp
@@ -0,0 +1,16 @@
+(ql:quickload 'sdl2)
+(ql:quickload 'cl-opengl)
+(ql:quickload '3d-matrices)
+
+(progn
+  (cffi:use-foreign-library "libGLEW.so")
+  (cffi:use-foreign-library "libGLU.so")
+  (cffi:use-foreign-library "libGL.so")
+  (cffi:use-foreign-library "libGLX.so")
+  (cffi:use-foreign-library "libSDL_image-1.2.so.0")
+  (cffi:use-foreign-library "libstb.so.0"))
+
+(load "../utils/utils")
+(load "../utils/shader")
+(load "../utils/3d-cffi")
+(load "../utils/stb")
